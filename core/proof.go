272f980eae4100028feb9733b6d2f88cce3c4a6b
Eduard S <eduard@iden3.io>
2019-01-28 12:48:10 +0100

    Add json marshaler for Hash, Proof and Data

--- core/proof.go
+++ core/proof.go
@@ -1,9 +1,6 @@
 package core
 
 import (
-	"encoding/hex"
-	"encoding/json"
-
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/iden3/go-iden3/merkletree"
 	"github.com/iden3/go-iden3/utils"
@@ -18,15 +15,6 @@ type ProofOfClaimPartial struct {
 	Aux  *SetRootAux       `json:"aux" binding:"required"`
 }
 
-func (p *ProofOfClaimPartial) MarshalJSON() ([]byte, error) {
-	return json.Marshal(map[string]interface{}{
-		"mtp0": hex.EncodeToString(p.Mtp0.Bytes()),
-		"mtp1": hex.EncodeToString(p.Mtp1.Bytes()),
-		"root": hex.EncodeToString(p.Root[:]),
-		"aux":  p.Aux,
-	})
-}
-
 // SetRootAux is the auxiliary data to build the set root claim from a root in
 // a partial proof of claim.
 type SetRootAux struct {
@@ -37,20 +25,10 @@ type SetRootAux struct {
 
 // ProofOfClaim is a complete proof of a claim that includes all the proofs of
 // existence and non-existence for mutliple levels from the leaf of a tree to
-// the signed root of possibly another tree whose root.
+// the signed root of possibly another tree whose root binding:"required".
 type ProofOfClaim struct {
 	Proofs    []ProofOfClaimPartial  `json:"proofs" binding:"required"`
 	Leaf      *merkletree.Data       `json:"leaf" binding:"required"`
 	Date      uint64                 `json:"date" binding:"required"`
 	Signature *utils.SignatureEthMsg `json:"signature" binding:"required"` // signature of the Root of the Relay
-}
-
-func (p *ProofOfClaim) MarshalJSON() ([]byte, error) {
-	leafBytes := p.Leaf.Bytes()
-	return json.Marshal(map[string]interface{}{
-		"proofs":    p.Proofs,
-		"leaf":      hex.EncodeToString(leafBytes[:]),
-		"date":      p.Date,
-		"signature": p.Signature,
-	})
 }
