014764b1e93218d29fa1d97f5780f7c4e0ea7c3f
Eduard S <eduard@iden3.io>
2019-02-22 17:47:06 +0100

    Use signed packet in vinculateId in nameserver

--- core/signed_packet.go
+++ core/signed_packet.go
@@ -27,19 +27,20 @@ func init() {
 
 const SIGV01 = "iden3.sig.v0_1"
 const IDENASSERTV01 = "iden3.iden_assert.v0_1"
+const GENERICSIGV01 = "iden3.gen_sig.v0_1"
 const SIGALGV01 = "EK256K1"
 
-// JwsHeader is the JSON Web Signature Header
-type JwsHeader struct {
-	Type         string         `json:"type" binding:"required"`
+// SigHeader is the JSON Web Signature Header of a signed packet
+type SigHeader struct {
+	Type         string         `json:"typ" binding:"required"`
 	Issuer       common.Address `json:"iss" binding:"required"`
 	IssuedAtTime int64          `json:"iat" binding:"required"`
 	Expiration   int64          `json:"exp" binding:"required"`
 	Algorithm    string         `json:"alg" binding:"required"`
 }
 
-// JwsPayload is the JSON Web Signature Payload
-type JwsPayload struct {
+// SigPayload is the JSON Web Signature Payload of a signed packet
+type SigPayload struct {
 	Type       string           `json:"type" binding:"required"`
 	Data       interface{}      `json:"data" binding:"required"`
 	KSign      *utils.PublicKey `json:"ksign" binding:"required"`
@@ -47,33 +48,53 @@ type JwsPayload struct {
 	Form       interface{}      `json:"form" binding:"required"`
 }
 
-// Jws is a JSON Web Signature unmarshaled packet
+// SignedPacket is a JSON Web Signature unmarshaled packet of a signed packet
+type SignedPacket struct {
+	Header      SigHeader
+	Payload     SigPayload
+	SignedBytes []byte
+	Signature   *utils.SignatureEthMsg
+}
+
+func (sp *SignedPacket) Sign(ks *keystore.KeyStore, addr common.Address) error {
+	headerJSON, err := json.Marshal(sp.Header)
+	if err != nil {
+		return err
+	}
+	payloadJSON, err := json.Marshal(sp.Payload)
+	if err != nil {
+		return err
+	}
+	sp.SignedBytes = []byte(fmt.Sprintf("%v.%v", base64.StdEncoding.EncodeToString([]byte(headerJSON)),
+		base64.StdEncoding.EncodeToString([]byte(payloadJSON))))
+	sp.Signature, err = utils.SignEthMsg(ks, accounts.Account{Address: addr}, sp.SignedBytes)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
 type Jws struct {
-	Header    JwsHeader
-	Payload   JwsPayload
-	Signature utils.SignatureEthMsg
+	Jws string `json:"jws" binding:"required"`
 }
 
-func (j *Jws) MarshalSign(ks *keystore.KeyStore, addr common.Address) (string, error) {
-	headerJSON, err := json.Marshal(j.Header)
-	if err != nil {
-		return "", err
+func (sp *SignedPacket) Marshal() (string, error) {
+	if sp.Signature == nil {
+		return "", fmt.Errorf("signed packet has not been signed yet")
 	}
-	payloadJSON, err := json.Marshal(j.Payload)
-	if err != nil {
-		return "", err
-	}
-	dataToSign := fmt.Sprintf("%v.%v", base64.StdEncoding.EncodeToString([]byte(headerJSON)),
-		base64.StdEncoding.EncodeToString([]byte(payloadJSON)))
-	sig, err := utils.SignEthMsg(ks, accounts.Account{Address: addr}, []byte(dataToSign))
-	if err != nil {
-		return "", err
-	}
-	sig64 := base64.StdEncoding.EncodeToString(sig[:])
-	return fmt.Sprintf("%v.%v", dataToSign, sig64), nil
+	sig64 := base64.StdEncoding.EncodeToString(sp.Signature[:])
+	return fmt.Sprintf("%v.%v", string(sp.SignedBytes), sig64), nil
 }
 
-func (j *Jws) Unmarshal(s string) error {
+func (sp *SignedPacket) MarshalJSON() ([]byte, error) {
+	str, err := sp.Marshal()
+	if err != nil {
+		return nil, err
+	}
+	return json.Marshal(Jws{Jws: str})
+}
+
+func (sp *SignedPacket) Unmarshal(s string) error {
 	fields := strings.Split(s, ".")
 	if len(fields) != 3 {
 		return fmt.Errorf("Invalid JWT: it doesn't contain 3 dot separated fields")
@@ -81,46 +102,57 @@ func (j *Jws) Unmarshal(s string) error {
 	jwsHeader64, jwsPayload64, signature64 := fields[0], fields[1], fields[2]
 	jwsHeader, err := base64.StdEncoding.DecodeString(jwsHeader64)
 	jwsPayload, err := base64.StdEncoding.DecodeString(jwsPayload64)
-	if err := json.Unmarshal(jwsHeader, &j.Header); err != nil {
+	if err := json.Unmarshal(jwsHeader, &sp.Header); err != nil {
 		return err
 	}
-	if err := json.Unmarshal(jwsPayload, &j.Payload); err != nil {
+	if err := json.Unmarshal(jwsPayload, &sp.Payload); err != nil {
 		return err
 	}
 	signature, err := base64.StdEncoding.DecodeString(signature64)
 	if err != nil {
 		return err
 	}
-	copy(j.Signature[:], signature)
+	sp.Signature = &utils.SignatureEthMsg{}
+	copy(sp.Signature[:], signature)
+	sp.SignedBytes = []byte(s[:strings.LastIndex(s, ".")])
 	return nil
 }
 
-func SignPacketV01(ks *keystore.KeyStore, idAddr common.Address, kSignPk *ecdsa.PublicKey, proofKSign ProofClaim,
-	expireDelta int64, payloadType string, data interface{}, form interface{}) (string, error) {
+func (sp *SignedPacket) UnmarshalJSON(bs []byte) error {
+	var jws Jws
+	if err := json.Unmarshal(bs, &jws); err != nil {
+		return err
+	}
+	sp.Unmarshal(jws.Jws)
+	return nil
+}
+
+func NewSignPacketV01(ks *keystore.KeyStore, idAddr common.Address, kSignPk *ecdsa.PublicKey,
+	proofKSign ProofClaim, expireDelta int64, payloadType string,
+	data interface{}, form interface{}) (*SignedPacket, error) {
 	now := time.Now().Unix()
-	header := JwsHeader{
+	header := SigHeader{
 		Type:         SIGV01,
 		Issuer:       idAddr,
 		IssuedAtTime: now,
 		Expiration:   now + expireDelta,
 		Algorithm:    SIGALGV01,
 	}
-	payload := JwsPayload{
+	payload := SigPayload{
 		Type:       IDENASSERTV01,
 		Data:       data,
 		KSign:      &utils.PublicKey{PublicKey: *kSignPk},
 		ProofKSign: proofKSign,
 		Form:       form,
 	}
-	jws := Jws{Header: header, Payload: payload}
-	signedPacket, err := jws.MarshalSign(ks, crypto.PubkeyToAddress(*kSignPk))
-	if err != nil {
-		return "", err
+	jws := SignedPacket{Header: header, Payload: payload}
+	if err := jws.Sign(ks, crypto.PubkeyToAddress(*kSignPk)); err != nil {
+		return nil, err
 	}
-	return signedPacket, nil
+	return &jws, nil
 }
 
-func VerifySignedPacketV01(signedPacket string, jws Jws) error {
+func VerifySignedPacketV01(jws *SignedPacket) error {
 	// 2. Verify jwsHeader.alg is 'ES255'
 	if jws.Header.Algorithm != SIGALGV01 {
 		return fmt.Errorf("Unsupported alg: %v", jws.Header.Algorithm)
@@ -128,8 +160,10 @@ func VerifySignedPacketV01(signedPacket string, jws Jws) error {
 
 	// 3. Verify that jwsHeader.iat <= now() < jwsHeader.exp
 	now := time.Now().Unix()
-	if !((jws.Header.IssuedAtTime <= now) && (now < jws.Header.Expiration)) {
-		return fmt.Errorf("Signature not valid for current date")
+	// Moving iat 2 minutes in the past to accomodate time shifts in time synchronization.
+	if !((jws.Header.IssuedAtTime-120 <= now) && (now < jws.Header.Expiration)) {
+		return fmt.Errorf("Signature not valid for current date (iat:%v, now:%v, exp:%v)",
+			jws.Header.IssuedAtTime, now, jws.Header.Expiration)
 	}
 
 	// 4. Verify that jwsPayload.ksign is in jwsPayload.proofKSign.leaf
@@ -159,9 +193,8 @@ func VerifySignedPacketV01(signedPacket string, jws Jws) error {
 	// As verifying a signature is cheaper than verifying a merkle tree
 	// proof, first we verify signature with ksign, and then we verify the
 	// merkle tree proofs.
-	msg := signedPacket[:strings.LastIndex(signedPacket, ".")]
 	if !utils.VerifySigEthMsg(crypto.PubkeyToAddress(jws.Payload.KSign.PublicKey),
-		&jws.Signature, []byte(msg)) {
+		jws.Signature, jws.SignedBytes) {
 		return fmt.Errorf("JWS signature doesn't match with pub key in payload.ksign")
 	}
 
@@ -173,15 +206,11 @@ func VerifySignedPacketV01(signedPacket string, jws Jws) error {
 	return nil
 }
 
-func VerifySignedPacket(signedPacket string) error {
-	var jws Jws
-	if err := jws.Unmarshal(signedPacket); err != nil {
-		return err
-	}
+func VerifySignedPacket(jws *SignedPacket) error {
 	switch jws.Header.Type {
 	// 1. Verify jwsHeader.typ is 'iden3.sig.v0_1'
 	case SIGV01:
-		return VerifySignedPacketV01(signedPacket, jws)
+		return VerifySignedPacketV01(jws)
 	default:
 		return fmt.Errorf("Unsupported signature packet typ: %v", jws.Header.Type)
 	}
