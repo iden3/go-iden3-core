8adfd3030e9794d5330a8f3afdf9446cd35db3cf
krlosMata <krlosmata@gmail.com>
2019-03-25 17:00:09 +0100

    updating handlers name server

--- cmd/claimserver/endpoint/claim.go
+++ cmd/claimserver/endpoint/claim.go
@@ -1,18 +1,19 @@
 package endpoint
 
 import (
-	"errors"
 
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/gin-gonic/gin"
 	"github.com/iden3/go-iden3/cmd/genericserver"
 	common3 "github.com/iden3/go-iden3/common"
 	"github.com/iden3/go-iden3/core"
+	"github.com/iden3/go-iden3/utils"
 
 	"github.com/iden3/go-iden3/merkletree"
 )
 
 type claimData struct {
-	IndexData string
+	IdAddr  	common.Address
 	Data      string
 }
 
@@ -21,46 +22,25 @@ func handlePostClaim(c *gin.Context) {
 	var m claimData
 	c.BindJSON(&m)
 
-	indexData, err := common3.HexDecode(m.IndexData)
-	if err != nil {
-		genericserver.Fail(c, "error on handlePostClaim", err)
-		return
-	}
 	data, err := common3.HexDecode(m.Data)
 	if err != nil {
 		genericserver.Fail(c, "error on handlePostClaim", err)
 		return
 	}
 
-	if len(indexData) < 400/8 {
-		genericserver.Fail(c, "error on handlePostClaim", errors.New("indexData smaller than 400/8"))
-		return
-	}
-	if len(data) < 496/8 {
-		genericserver.Fail(c, "error on handlePostClaim", errors.New("data smaller than 496/8"))
-		return
-	}
+	var nameHash [256 / 8]byte
+	hash := utils.HashBytes([]byte(data))
+	copy(nameHash[:], hash[len(hash)-256/8:])
+	claim := core.NewClaimLinkObjectIdentity(0, 5, 0, m.IdAddr, nameHash)
 
-	var indexSlot [400 / 8]byte
-	var dataSlot [496 / 8]byte
-	copy(indexSlot[:], indexData[:400/8])
-	copy(dataSlot[:], data[:496/8])
-	claim := core.NewClaimBasic(indexSlot, dataSlot)
-	err = genericserver.Claimservice.AddDirectClaim(*claim)
+	err = genericserver.Claimservice.AddLinkObjectClaim(*claim)
 	if err != nil {
-		genericserver.Fail(c, "error on AddDirectClaim", err)
-		return
-	}
-
-	// return claim with proofs
-	proofOfClaim, err := genericserver.Claimservice.GetClaimProofByHi(claim.Entry().HIndex())
-	if err != nil {
-		genericserver.Fail(c, "error on GetClaimByHi", err)
+		genericserver.Fail(c, "error on AddLinkObjectClaim", err)
 		return
 	}
 
 	c.JSON(200, gin.H{
-		"proofClaim": proofOfClaim,
+		"status": "ok",
 	})
 	return
 }
