a6d45c22bb4e107004f256c862f732e8a18d8bc2
Eduard S <eduard@iden3.io>
2020-04-23 16:47:07 +0200

    Integrate zk proof in iden state update

--- components/idenpubonchain/local/local_test.go
+++ components/idenpubonchain/local/local_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 	"time"
 
+	zktypes "github.com/iden3/go-circom-prover-verifier/types"
 	"github.com/iden3/go-iden3-core/components/idenpubonchain"
 	"github.com/stretchr/testify/require"
 )
@@ -11,12 +12,14 @@ import (
 // Assert that IdenPubOnChain follows the IdenPubOnChainer interface
 func TestLocalIdenPubOnChainInterface(t *testing.T) {
 	var idenPubOnChain idenpubonchain.IdenPubOnChainer //nolint:gosimple
-	idenPubOnChain = New(func() time.Time {
-		return time.Now()
-	},
+	idenPubOnChain = New(
+		func() time.Time {
+			return time.Now()
+		},
 		func() uint64 {
 			return 0
 		},
+		&zktypes.Vk{},
 	)
 	require.NotNil(t, idenPubOnChain)
 }
