1dcbfdda8b90e1e59546360e89ba0adc91bd1509
vmidyllic <74898029+vmidyllic@users.noreply.github.com>
2022-12-02 17:00:47 +0200

    Hex() FromHex() methods

--- claim.go
+++ claim.go
@@ -791,6 +791,15 @@ func (c *Claim) UnmarshalJSON(in []byte) error {
 	return nil
 }
 
+// Hex returns hex representation of binary claim
+func (c Claim) Hex() (string, error) {
+	b, err := c.MarshalBinary()
+	if err != nil {
+		return "", err
+	}
+	return hex.EncodeToString(b), err
+}
+
 func (c Claim) MarshalBinary() ([]byte, error) {
 	var buf = bytes.NewBuffer(nil)
 	buf.Grow(len(c.index)*len(c.index[0]) + len(c.value)*len(c.value[0]))
@@ -828,6 +837,15 @@ func (c *Claim) UnmarshalBinary(data []byte) error {
 	}
 
 	return nil
+}
+
+func (c *Claim) FromHex(hexStr string) error {
+
+	data, err := hex.DecodeString(hexStr)
+	if err != nil {
+		return err
+	}
+	return c.UnmarshalBinary(data)
 }
 
 // GetMerklizedPosition returns the position at which the Merklize flag is stored.
