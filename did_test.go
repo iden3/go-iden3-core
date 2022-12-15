0100e1f0831463e16b757ba7146f611bf8017a07
Dmytro Sukhyi <dmytro.sukhiy@gmail.com>
2022-12-15 13:08:56 +0200

    Test PolygonID_Types

--- did_test.go
+++ did_test.go
@@ -80,3 +80,44 @@ func TestDIDGenesisFromState(t *testing.T) {
 	require.Equal(t, NoNetwork, did.NetworkID)
 	require.Equal(t, "did:iden3:tJ93RwaVfE1PEMxd5rpZZuPtLCwbEaDCrNBhAy8HM", did.String())
 }
+
+func TestDID_PolygonID_Types(t *testing.T) {
+
+	// Polygon no chain, no network
+	did := helperBuildDIDFromType(t, DIDMethodPolygonID, NoChain, NoNetwork)
+
+	require.Equal(t, DIDMethodPolygonID, did.Method)
+	require.Equal(t, NoChain, did.Blockchain)
+	require.Equal(t, NoNetwork, did.NetworkID)
+	require.Equal(t, "did:polygonID:2mbH5rt9zKT1mTivFAie88onmfQtBU9RQhjNPLwFZh", did.String())
+
+	// Polygon | Polygon chain, Main
+	did2 := helperBuildDIDFromType(t, DIDMethodPolygonID, Polygon, Main)
+
+	require.Equal(t, DIDMethodPolygonID, did2.Method)
+	require.Equal(t, Polygon, did2.Blockchain)
+	require.Equal(t, Main, did2.NetworkID)
+	require.Equal(t, "did:polygonID:polygon:main:2pzr1wiBm3Qhtq137NNPPDFvdk5xwRsjDFnMxpnYHm", did2.String())
+
+	// Polygon | Polygon chain, Mumbai
+	did3 := helperBuildDIDFromType(t, DIDMethodPolygonID, Polygon, Mumbai)
+
+	require.Equal(t, DIDMethodPolygonID, did3.Method)
+	require.Equal(t, Polygon, did3.Blockchain)
+	require.Equal(t, Mumbai, did3.NetworkID)
+	require.Equal(t, "did:polygonID:polygon:mumbai:2qCU58EJgrELNZCDkSU23dQHZsBgAFWLNpNezo1g6b", did3.String())
+
+}
+
+func helperBuildDIDFromType(t testing.TB, method DIDMethod, blockchain Blockchain, network NetworkID) *DID {
+	t.Helper()
+
+	typ, err := BuildDIDType(method, blockchain, network)
+	require.NoError(t, err)
+
+	genesisState := big.NewInt(1)
+	did, err := DIDGenesisFromIdenState(typ, genesisState)
+	require.NoError(t, err)
+
+	return did
+}
